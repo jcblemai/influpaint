31f0dc7fdcccf29450e7cc4830c36bdcf84b8763
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-11-01 18:01:21 +0100

    polishing data_classes

--- data_classes.py
+++ data_classes.py
@@ -5,101 +5,68 @@ import xarray as xr
 import data_utils
 
 
-class FluDynamicsDataset1D(torch.utils.data.Dataset):
-    def __init__(self, dataset_type, flusetup, download=False, transform=None):
+class FluDataset(torch.utils.data.Dataset):
+    def __init__(self, flu_dyn, transform=None, transform_inv=None, channels=3):
         """
         Args:
-            transform (callable, optional): Optional transform to be applied
-            on a sample.
+            flu_dyn (np.array): flu dynamics, shape (n_samples, n_features, n_dates, n_places)
         """
-        self.flusetup = flusetup
-        self.dataset_type = dataset_type
         self.transform = transform
+        self.transform_inv = transform_inv
 
-        if dataset_type == "onlyfluview":
-            self.samples = []
-            fluview = data_utils.get_from_epidata(dataset="fluview", flusetup=self.flusetup, download=download, write=False)
-            df = fluview[fluview['location_code'].isin(flusetup.locations)]
-                
-        #elif dataset_type == "all":
-        #    self.flu_dyn = {
-        #        "flusurv": data_utils.get_from_epidata(dataset="flusurv", download=download, write=False),
-        #        "fluview": data_utils.get_from_epidata(dataset="fluview", download=download, write=False),
-        #        "flusight": data_utils.get_from_epidata(dataset="flusight", download=download, write=False),
-        #    }
+        self.flu_dyn = flu_dyn
+        #  self.max_per_feature = self.flu_dyn.max(dim=["date", "place", "sample"])
+        self.max_per_feature = np.max(self.flu_dyn, axis=(0,2,3)) # TODO: Check with channels, also perhaps use keepdims=True for broadcasting
 
-        self.samples = data_utils.dataframe_to_arraylist(df=df, flusetup=self.flusetup)
-        
-        #if channels = 1:
-        #    self.flu_dyn=self.flu_dyn.isel(feature=0)
-        
-        # let's store the min and max
-        #self.max_per_feature = self.flu_dyn.max(dim=["date", "place", "sample"])
-        #print(f"created dataset with scale {np.array(self.max_per_feature)}")
-        #self.flu_dyn_norm = (np.sqrt(self.flu_dyn)/np.sqrt(self.max_per_feature))*2#-1
-        
-        
-    def __len__(self):
-        return len(self.samples)
-    
-    def __getitem__(self, idx):
-        frame = self.getitem_nocast(idx)
-        return torch.from_numpy(frame).float()
-    
-    def getitem_nocast(self, idx):
-        if torch.is_tensor(idx):
-            idxl = idx.tolist()
-        else:
-            idxl=idx
-            
-        epi_frame = self.samples[idxl]#.astype(np.float32)
+        print(f"created dataset with max {np.array(self.max_per_feature)}, full dataset has shape {self.flu_dyn.shape}")
 
-        #shoudle be a dict ?
-        #sample = {'image': image, 'landmarks': landmarks}
-
-        if self.transform:
-            epi_frame = self.transform(epi_frame)
-
-        return epi_frame
-    
-    def unnormalized(self, array):
-        return array
-    
-    def test(self, idx):
-        """
-        test that we can transform and go back & get the same thing
-        """
-        epi_frame_n, idx_n = self.getitem_nocast(idx)
-        assert idx_n == idx
-        assert (np.abs(self.unnormalized(epi_frame_n) - self.flu_dyn.sel(sample=idx)) < 1e-3).all()
-
-
-class FluDynamicsSyntheticDatasetLegacy(torch.utils.data.Dataset):
-    def __init__(self, netcdf_file, transform=None, channels=3):
-        """
-        Args:
-            netcdf_file (string): Path to the netcdf file with the xarray data
-            transform (callable, optional): Optional transform to be applied
-            on a sample.
-        """
-        self.flu_dyn = xr.open_dataarray(netcdf_file)
-        self.transform = transform
-        
+    @classmethod
+    def from_csp_SMHR1(cls, netcdf_file, 
+                    transform=None, 
+                    transform_inv=None,
+                    channels=3):
+        flu_dyn = xr.open_dataarray(netcdf_file)
         if channels == 1:
-            flu_dyn = self.flu_dyn.sel(feature="incidH_FluA")+ self.flu_dyn.sel(feature="incidH_FluB")
-            self.flu_dyn = flu_dyn.expand_dims('feature', axis=1).assign_coords(feature=('feature', ['incidH']))
-        
-        # let's store the min and max
-        self.max_per_feature = self.flu_dyn.max(dim=["date", "place", "sample"])
-        self.f
-        print(f"created dataset with scale {np.array(self.max_per_feature)}")
-        self.flu_dyn_norm = (np.sqrt(self.flu_dyn)/np.sqrt(self.max_per_feature))*2#-1
+            flu_dyn = flu_dyn.sel(feature="incidH_FluA")+ flu_dyn.sel(feature="incidH_FluB")
+            flu_dyn = flu_dyn.expand_dims('feature', axis=1).assign_coords(feature=('feature', ['incidH']))
+        flu_dyn = flu_dyn.data
+        return cls(flu_dyn=flu_dyn, transform=transform, transform_inv=transform_inv, channels=channels)
 
-    def add_transform(self, transform):
+    @classmethod
+    def from_flusurvCSP(cls, 
+                    flusetup,
+                    transform=None, 
+                    transform_inv=None, 
+                    channels=3):
+        csp_flusurv = pd.read_csv("datasets/flu_surv_cspGT.csv", parse_dates=["date"])
+        df = pd.merge(csp_flusurv, flusetup.locations_df, left_on="FIPS", right_on="abbreviation", how='left')
+        df["fluseason"]= df["date"].apply(flusetup.get_fluseason_year)
+        df["fluseason_fraction"]= df["date"].apply(flusetup.get_fluseason_fraction)
+        flu_dyn = np.array(data_utils.dataframe_to_arraylist(df, flusetup = flusetup, value_column='incidH'))
+        return cls(flu_dyn=flu_dyn, transform=transform, transform_inv=transform_inv, channels=channels)
+
+    @classmethod
+    def from_fluview(cls, 
+                    flusetup,
+                    download=False,
+                    transform=None, 
+                    transform_inv=None, 
+                    channels=3):
+        fluview = data_utils.get_from_epidata(dataset="fluview", flusetup=flusetup, download=download, write=False)
+        df = fluview[fluview['location_code'].isin(flusetup.locations)]
+        flu_dyn = np.array(data_utils.dataframe_to_arraylist(df=df, flusetup=flusetup))
+
+        return cls(flu_dyn=flu_dyn, transform=transform, transform_inv=transform_inv, channels=channels)
+
+
+    def add_transform(self, transform, transform_inv):
         self.transform = transform
+        self.transform_inv = transform_inv
+        # test that the inverse transform really works
+        self.test(0)
         
     def __len__(self):
-        return len(self.flu_dyn.sample)
+        return self.flu_dyn.shape[0]
     
     def __getitem__(self, idx):
         frame = self.getitem_nocast(idx)
@@ -110,36 +77,32 @@ class FluDynamicsSyntheticDatasetLegacy(torch.utils.data.Dataset):
             idxl = idx.tolist()
         else:
             idxl=idx
-            
         # should be squeezed when channel = 3 ?
-        epi_frame = self.flu_dyn_norm.sel(sample=idxl).to_numpy()#.astype(np.float32)
-
-        #shoudle be a dict ?
-        #sample = {'image': image, 'landmarks': landmarks}
-
-        if self.transform:
-            epi_frame = self.transform(epi_frame)
-
+        epi_frame = self.flu_dyn[idxl,:,:,:]
+        epi_frame = self.apply_transform(epi_frame)
         return epi_frame
+
+    def apply_transform(self, epi_frame):
+        if self.transform:
+            array = self.transform(epi_frame)
+            return array
+        else:
+            return epi_frame
     
-    def unnormalized(self, array):
-        #return (((array)/3)*np.sqrt((self.max_per_feature.to_numpy())[:,np.newaxis, np.newaxis]))**2
-        return (((array)/2)*np.sqrt((self.max_per_feature.to_numpy())[:,np.newaxis, np.newaxis]))**2
-        #return ((array+.75)*np.sqrt((self.max_per_feature.to_numpy())[:,np.newaxis, np.newaxis]))**2
+    def apply_transform_inv(self, epi_frame):
+        if self.transform_inv:
+            array = self.transform_inv(epi_frame)
+            return array
+        else:
+            return epi_frame
     
     def test(self, idx):
         """
         test that we can transform and go back & get the same thing
         """
         epi_frame_n = self.getitem_nocast(idx)
-        #print(self.unnormalized(epi_frame_n).shape, self.unnormalized(epi_frame_n)[0,0,0])
-        #print(self.flu_dyn.sel(sample=idx).shape, self.flu_dyn.sel(sample=idx)[0,0,0])
-        assert (np.abs(self.unnormalized(epi_frame_n) - self.flu_dyn.sel(sample=idx)) < 1e-3).all()
-        print("test passed: back and forth transformation is ok ✅")
-
-
-    
-    
+        assert (np.abs(self.apply_transform_inv(epi_frame_n) - self.flu_dyn[idx]) < 1e5).all()
+        print("test passed: back and forth transformation are ok ✅")
 
 # These transform applies to a numpy object with dimensions
 #  (feature, date, place)
