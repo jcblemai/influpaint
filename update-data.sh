0874c9148f93a49bb9993c8a7d04060143379b82
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-11-10 18:55:54 +0100

    datasets features

--- update-data.sh
+++ update-data.sh
@@ -2,10 +2,12 @@ cd datasets/Flusight-forecast-data
 echo " ⚠️ ⚠️ ⚠️  Make sure fork jcblemai/Flusight-forecast-data is synced cdcepi/Flusight-forecast-data !!!"
 git pull
 cd ../..
+
 cd datasets/delphi-epidata
 git pull
 cd ../..
 cp datasets/delphi-epidata/src/client/delphi_epidata.py helpers/
-cd datasets/synthethic/flu-scenario-modeling-hub
+
+cd datasets/synthetic/flu-scenario-modeling-hub/
 git pull
 cd ../../..
