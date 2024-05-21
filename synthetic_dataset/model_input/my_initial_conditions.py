d7db63720b7bfad3c7af9cb564a1c72e7732e5a0
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-05-21 17:27:12 +0200

    flepimop files

---
+++ synthetic_dataset/model_input/my_initial_conditions.py
@@ -0,0 +1,20 @@
+import gempyor.initial_conditions
+import numpy as np
+
+class InitialConditions(gempyor.initial_conditions.InitialConditions):
+
+    def get_from_config(self, sim_id: int, modinf) -> np.ndarray:
+        y0 = np.zeros((modinf.compartments.compartments.shape[0], modinf.nsubpops))
+        S_idx = modinf.compartments.get_comp_idx({"infection_stage":"S"})
+        I_idx = modinf.compartments.get_comp_idx({"infection_stage":"I"})
+        R_idx = modinf.compartments.get_comp_idx({"infection_stage":"R"})
+        prop_rec = np.random.uniform(low=.3,high=.8, size=modinf.nsubpops)
+        n_inf = 100
+        y0[S_idx, :] = modinf.subpop_pop * (1- prop_rec) - n_inf
+        y0[I_idx, :] = n_inf
+        y0[R_idx, :] = modinf.subpop_pop *  prop_rec 
+
+        return y0
+    
+    def get_from_file(self, sim_id: int, modinf) -> np.ndarray:
+        return self.draw(sim_id=sim_id, modinf=modinf)
