9620150e36db069901b59551eda18159838956d2
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-05-23 17:00:50 +0200

    working on dataset

--- synthetic_dataset/model_input/my_initial_conditions.py
+++ synthetic_dataset/model_input/my_initial_conditions.py
@@ -9,7 +9,7 @@ class InitialConditions(gempyor.initial_conditions.InitialConditions):
         I_idx = modinf.compartments.get_comp_idx({"infection_stage":"I"})
         R_idx = modinf.compartments.get_comp_idx({"infection_stage":"R"})
         prop_rec = np.random.uniform(low=.3,high=.8, size=modinf.nsubpops)
-        n_inf = 100
+        n_inf = modinf.subpop_pop*0.005
         y0[S_idx, :] = modinf.subpop_pop * (1- prop_rec) - n_inf
         y0[I_idx, :] = n_inf
         y0[R_idx, :] = modinf.subpop_pop *  prop_rec 
@@ -18,3 +18,8 @@ class InitialConditions(gempyor.initial_conditions.InitialConditions):
     
     def get_from_file(self, sim_id: int, modinf) -> np.ndarray:
         return self.draw(sim_id=sim_id, modinf=modinf)
+
+
+import gempyor.initial_conditions
+import numpy as np
+
